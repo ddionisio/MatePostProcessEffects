4a18d2d4b7844aba9afeaef6dce4dcc1096a3f65
David Dionisio <ddionisio@renegadeware.com>
2019-10-10 10:47:13 -0400

    screen wave

---
+++ Resources/Shaders/Wave.shader
@@ -0,0 +1,35 @@
+﻿Shader "Hidden/M8/PostProcessEffects/Wave"
+{
+	HLSLINCLUDE
+		#include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+
+		TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+
+		float4 parms; // x,y = amplitude; z,w = range
+		float2 speed;
+
+		float4 Frag(VaryingsDefault i) : SV_Target
+		{
+			float2 pos = float2(
+				i.texcoord.x + sin(i.texcoord.y * parms.z + speed.x * _Time.y) * parms.x,
+				i.texcoord.y + sin(i.texcoord.x * parms.w + speed.y * _Time.y) * parms.y);
+
+			float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, pos);
+
+			return color;
+		}
+	ENDHLSL
+	
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			HLSLPROGRAM
+				#pragma vertex VertDefault
+				#pragma fragment Frag
+			ENDHLSL
+		}
+	}
+}
---
+++ Scripts/Wave.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace M8.PostProcessEffects {
+    [System.Serializable]
+    [PostProcess(typeof(WaveRenderer), PostProcessEvent.BeforeStack, "M8/Wave")]
+    public class Wave : PostProcessEffectSettings {
+
+        [Tooltip("Intensity of the wave in X/Y. Typically in fraction.")]
+        public Vector2Parameter amplitude = new Vector2Parameter();
+
+        [Tooltip("Speed of the wave in X/Y.")]
+        public Vector2Parameter speed = new Vector2Parameter();
+
+        [Tooltip("Scale of the wave. Higher values allow more waves to fit on screen.")]
+        public Vector2Parameter range = new Vector2Parameter();
+    }
+
+    public class WaveRenderer : PostProcessEffectRenderer<Wave> {
+
+        private const float INV_2PI = 1f / (2f * Mathf.PI);
+
+        private int mParmID;
+        private int mSpeedID;
+
+        public override void Init() {
+            mParmID = Shader.PropertyToID("parms");
+            mSpeedID = Shader.PropertyToID("speed");
+        }
+
+        public override void Render(PostProcessRenderContext context) {            
+            var sheet = context.propertySheets.Get(Shader.Find("Hidden/M8/PostProcessEffects/Wave"));
+                        
+            sheet.properties.SetVector(mParmID, 
+                new Vector4(
+                    settings.amplitude.value.x * INV_2PI, settings.amplitude.value.y * INV_2PI, 
+                    settings.range.value.x, settings.range.value.y));
+
+            sheet.properties.SetVector(mSpeedID, settings.speed);
+
+            context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+        }
+    }
+}
